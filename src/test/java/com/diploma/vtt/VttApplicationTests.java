4e11a669345db064fd2adada613364fd8f53711e
Maria <marianmnk2001@yandex.ru>
2023-03-26 15:43:46 +0300

    Initial commit

---
+++ src/test/java/com/diploma/vtt/VttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.vtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
